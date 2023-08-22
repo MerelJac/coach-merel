37c73fa6e4fde57be7dacca0947808fe5a24ec35
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-22 15:08:49 -0700

    mySQL db connected to backend server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const sequelize = require('./config/connection');
+const routes = require('./controllers')
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+// app.use(routes)
 
 app.get('/', (req, res) => {
     return res.json('from backend')
 })
 
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
     console.log(`listening on port http://localhost:${PORT}`)
+})
 })

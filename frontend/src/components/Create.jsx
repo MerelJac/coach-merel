2fce177b7ed8488783130d87f181bcc9c929f661
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-29 13:15:47 -0700

    starting search functionality

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { ExerciseDiv } from "./ExerciseDiv";
 
+const searchFunction = (e) => {
+    const searchBar = document.querySelector('#create-search')
+    let searchValue = searchBar.value
+    console.log(searchValue)
+    // reset search bar 
+    searchBar.value = ''
+    searchBar.placeholder = 'Search'
+
+
+}
+
 export const Create = () => {
     return (
         <>
         <div>
             <h1 className="right-align">Start<span className="bold">NewWorkout</span></h1>
         </div>
-
-        <input placeholder="Search"></input>
-
+        <div>
+            {/* TODO on submit isnt working? */}
+            <input id="create-search" type="search" placeholder="Search" onSubmit={searchFunction}></input>
+            <button onClick={searchFunction}>Search</button>
+        </div>
         {< ExerciseDiv />}
 
         {/* SHOULD BE exercise COMPONENT */}

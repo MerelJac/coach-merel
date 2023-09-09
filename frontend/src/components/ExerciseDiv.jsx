b0b26194ed845c470ac777f139e3c9a9555210eb
Merel Jacobs <merel.burleigh@gmail.com>
2023-09-08 17:20:33 -0700

    set info prints to page

--- frontend/src/components/ExerciseDiv.jsx
+++ frontend/src/components/ExerciseDiv.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-import dotsImg from '../assets/images/dots.jpg'
+import dotsImg from "../assets/images/dots.jpg";
 
+export const ExerciseDiv = ({ title, setArray }) => {
+  const [sets, setSets] = useState([]);
 
-export const ExerciseDiv = ({ title }) => {
+  // submit set info to page
+  const setInfo = (e) => {
+    e.preventDefault();
+    // find elements
+    let reps = document.querySelector(".reps").value;
+    let weight = document.querySelector(".weight").value;
+    // array of sets
+    setSets([...sets, `${weight}lbs x ${reps}`]);
+  };
 
-    return (
+  return (
     <>
-    <section className="row">
+      <section className="row">
         <img alt="attributeImg" src={dotsImg}></img>
         <div className="exercise-text">
-            <h2 className="bold">{title}</h2>
+          <h2 className="bold">{title}</h2>
         </div>
         <div>
-            <input className="exercise-input" placeholder="reps"></input>
-            <input className="exercise-input" placeholder="lbs"></input>
+          <input className="exercise-input reps" placeholder="reps"></input>
+          <input className="exercise-input weight" placeholder="lbs"></input>
         </div>
-
-    </section>
+        <button className="submitRep" type="submit" onClick={setInfo}>
+          Go
+        </button>
+      </section>
+      <section className="set-print-section">
+        <p>{sets}</p>
+      </section>
     </>
-    )
-}
+  );
+};

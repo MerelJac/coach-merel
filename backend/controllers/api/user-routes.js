e80e4e033c87921e26824e9481d2fc177297ed4b
Merel Jacobs <merel.burleigh@gmail.com>
2023-09-13 20:27:05 -0700

    working on routes

--- backend/controllers/api/user-routes.js
+++ backend/controllers/api/user-routes.js
@@ -1,92 +1,56 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // goes to folder / index file, not direct path
-const { User } = require('../../models')
+const { User } = require("../../models");
 
-// GET ALL users
-router.get('/', async (req, res) => {
-    const allUsers = await User.findAll()
-    res.json(allUsers)
-})
+// GET ALL users - works
+router.get("/", async (req, res) => {
+  const allUsers = await User.find({});
+  res.json(allUsers);
+});
 
-// CREATE new user
-router.post('/', async (req, res) => {
-    try {
-        let newUser = await User.create(req.body);
-        console.log(newUser)
-        console.log('new user was created: ' + newUser)
-        console.log(newUser.id)
-
-        //save session
-          req.session.save(() => {
-          req.session.loggedIn = true;
-          req.session.user = newUser;
-          console.log(req.session)
-
-        res.status(200).json({ message: "You are now logged in!"})
-        })
-    } catch (error) {
-        if (error.name == 'SequelizeUniqueConstraintError') {
-            return res.status(400).json({ error: 'Email address must be unique.' });
-          }
-
-          console.error(error);
-          return res.status(500).json({ error: 'An error occurred while creating the user.' });
-            }
-        }
-    )
-
-// test endpoint
-router.get('/test', (req, res) => {
-  console.log('TESTING TESTING', req.session)
-  res.json(req.session)
-})
+// CREATE new user -- works 
+router.post("/", async (req, res) => {
+  try {
+    let newUser = await User.create(req.body);
+    console.log("new user was created: " + newUser);
+    res.json(newUser);
+    // TODO - input logic to save session
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // Login / Set Session
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
-    // includes password validation
-    let userData = await User.findOne( {
-      where: {
-        email: req.body.email,
-        password: req.body.password
-      }, 
-      attributes: {exclude: ["password"]}
-    } 
-    );
-    console.log(userData)
-    console.log('new user was created: ' + userData)
-    console.log(userData.id)
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password }).select('-password');
 
-    //save session
-      req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.user = userData;
-      console.log(req.session.loggedIn, req.session.user)
-      console.log(req.session)
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password.' });
+    }
+    // TODO - sesssion save 
 
-    res.status(200).json({ message: "You are now logged in!", userData})
-    })
-} catch (error) {
-  // TODO create unique error handling
-      console.error(error);
-      return res.status(500).json({ error: 'An error occurred while creating the user.' });
-        } 
+    res.status(200).json({ message: 'You are now logged in!', user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'An error occurred while logging in.' });
   }
-)
+});
 
-// Validate logged In User 
-router.get('/me', (req, res) => {
-  console.log("peanut butter")
-  console.log("WE ARE HERE WE AREW HERE", req.session)
+// Validate logged In User
+router.get("/me", (req, res) => {
+  console.log("peanut butter");
+  console.log("WE ARE HERE WE AREW HERE", req.session);
   // TODO req.session isnt persisting?
   try {
     let user = req.session.user;
-    console.log('User: ', user);
-    res.status(200).json(user)
-  } catch(err) {
-    console.error(err)
-    res.sendStatus(err)
+    console.log("User: ", user);
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(err);
   }
-})
+});
 
 module.exports = router;
--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,10 @@ const userSchema = new Schema({
     type: String, required: true 
 },
   email: { 
-    type: String, required: true },
+    type: String, 
+    required: true,
+    unique: true
+},
   password: { 
     type: String, required: true },
 }, {

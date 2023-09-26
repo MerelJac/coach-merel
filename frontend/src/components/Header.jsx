c4e037f6668feb468b440ceb6000596c952ecfa8
Merel Jacobs <merel.burleigh@gmail.com>
2023-09-26 12:54:35 -0700

    updated header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 // TODO should include user info eventally
 
-const returnHome = () => {
-    window.location.href = '/'
-} 
 
-const accountInfo = () => {
-    window.location.href = '/account-info'
-}
 export const Header = (props) => {
-    return (
-        <>
-        <div className="header">
-            <h1 onClick={returnHome}>Title<span className="bold">Title</span></h1>
-            <p onClick={accountInfo}>User</p>
-        </div>
-        </>
-    )
-}
+  const navigate = useNavigate();
+  const returnHome = () => {
+    navigate("/");
+  };
+
+  const accountInfo = () => {
+    navigate("/account-info");
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1 onClick={returnHome}>
+          Title<span className="bold">Title</span>
+        </h1>
+        <p onClick={accountInfo}>Account</p>
+      </div>
+    </>
+  );
+};

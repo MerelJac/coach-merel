f002d9c7d8ac5c0edbceae6aab50f2221e6f12a5
Jen Sandage <jensandage1@gmail.com>
2023-10-12 21:34:04 -0700

    added add and remove exercise mutations to client utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,3 +10,31 @@ export const LOGIN = gql`
     }
   }
 `;
+
+export const ADD_EXERCISE = gql`
+  mutation AddExercise($userId: ID!, $name: String!, $oneRepMax: Int!, $category: String, $notes: String) {
+    addExercise(userId: $userId, name: $name, oneRepMax: $oneRepMax, category: $category, notes: $notes) {
+      _id
+      name
+      oneRepMax
+      category
+      notes
+      dateAdded
+      userId
+    }
+  }
+`;
+
+export const REMOVE_EXERCISE = gql`
+  mutation RemoveExercise($exerciseId: ID!, $userId: ID!) {
+    removeExercise(exerciseId: $exerciseId, userId: $userId) {
+      _id
+      name
+      oneRepMax
+      category
+      notes
+      dateAdded
+      userId
+    }
+  }
+`;

7b4d505a933e7177dd277085214bec6dbe34a96a
Brendan Aper <brendanaper@gmail.com>
2023-10-16 22:03:36 -0700

    fixed again

--- client/src/scenes/pages/Create.jsx
+++ client/src/scenes/pages/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { ADD_EXERCISE } from "../utils/mutations";
+import { ADD_EXERCISE } from "../../utils/mutations";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 
